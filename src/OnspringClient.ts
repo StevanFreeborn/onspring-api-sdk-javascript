9edb4bd316667843cef49d588ef356476a94d4df
StevanFreeborn <sfreeborn7@gmail.com>
2023-01-25 17:08:19 -0600

    initial commit

---
+++ src/OnspringClient.ts
@@ -0,0 +1,9 @@
+class OnspringClient {
+  private readonly _baseUrl: string;
+  private readonly _apiKey: string;
+
+  constructor(baseUrl: string, apiKey: string) {
+    this._baseUrl = baseUrl;
+    this._apiKey = apiKey;
+  }
+}
