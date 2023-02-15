85db9293a9c5b24158cdc82242b99c204aeefcd0
StevanFreeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-02-15 00:01:07 -0600

    feat: begin writing files integration tests

---
+++ integrationTests/Files/getFileInfoById.spec.ts
@@ -0,0 +1,13 @@
+import { OnspringClient } from '../../src/index';
+import { expect } from 'chai';
+import { baseURL, apiKey } from '../mochaRootHooks';
+
+describe('getFileInfoById', function () {
+  it('should return information about a file in an attachment field', async function () {
+    expect.fail('Not implemented');
+  });
+
+  it('should return information about a file in an image field', async function () {
+    expect.fail('Not implemented');
+  });
+});
