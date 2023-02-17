2d345a6486c1ec4ad8c5762812b94e35fc071d09
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-02-17 12:26:24 -0600

    fix: add models and enums to package index as exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,136 @@
+// Models
+import { ApiResponse } from './models/ApiResponse';
+import { App } from './models/App';
+import { Attachment } from './models/Attachment';
+import { AttachmentListRecordValue } from './models/AttachmentListRecordValue';
+import { CollectionResponse } from './models/CollectionResponse';
+import { CreatedWithIdResponse } from './models/CreatedWithIdResponse';
+import { DateRecordValue } from './models/DateRecordValue';
+import { DecimalRecordValue } from './models/DecimalRecordValue';
+import { Delegate } from './models/Delegate';
+import { DelegateListRecordValue } from './models/DelegateListRecordValue';
+import { EndpointFactory } from './models/EndpointFactory';
+import { Field } from './models/Field';
+import { File } from './models/File';
+import { FileInfo } from './models/FileInfo';
+import { FileListRecordValue } from './models/FileListRecordValue';
+import { FormulaField } from './models/FormulaField';
+import { GetPagedAppsResponse } from './models/GetPagedAppsResponse';
+import { GetPagedFieldsResponse } from './models/GetPagedFieldsResponse';
+import { GetPagedRecordsResponse } from './models/GetPagedRecordsResponse';
+import { GetPagedReportsResponse } from './models/GetPagedReportsResponse';
+import { GetRecordRequest } from './models/GetRecordRequest';
+import { GetRecordsByAppIdRequest } from './models/GetRecordsByAppIdRequest';
+import { GetRecordsRequest } from './models/GetRecordsRequest';
+import { GuidListRecordValue } from './models/GuidListRecordValue';
+import { GuidRecordValue } from './models/GuidRecordValue';
+import { IntegerListRecordValue } from './models/IntegerListRecordValue';
+import { IntegerRecordValue } from './models/IntegerRecordValue';
+import { ListField } from './models/ListField';
+import { ListItemRequest } from './models/ListItemRequest';
+import { ListItemResponse } from './models/ListItemResponse';
+import { ListValue } from './models/ListValue';
 import { OnspringClient } from './models/OnspringClient';
+import { PagedResponse } from './models/PagedResponse';
 import { PagingRequest } from './models/PagingRequest';
+import { QueryFilter } from './models/QueryFilter';
+import { QueryRecordsRequest } from './models/QueryRecordsRequest';
+import { Record } from './models/Record';
+import { RecordValue } from './models/RecordValue';
+import { ReferenceField } from './models/ReferenceField';
+import { Report } from './models/Report';
+import { ReportData } from './models/ReportData';
+import { Row } from './models/Row';
 import { SaveFileRequest } from './models/SaveFileRequest';
+import { SaveRecordRequest } from './models/SaveRecordRequest';
+import { SaveRecordResponse } from './models/SaveRecordResponse';
+import { ScoringGroup } from './models/ScoringGroup';
+import { ScoringGroupListRecordValue } from './models/ScoringGroupListRecordValue';
+import { StringListRecordValue } from './models/StringListRecordValue';
+import { StringRecordValue } from './models/StringRecordValue';
+import { TimeSpanData } from './models/TimeSpanData';
+import { TimeSpanRecordValue } from './models/TimeSpanRecordValue';
 
-export { OnspringClient, PagingRequest, SaveFileRequest };
+// Enums
+import { DataFormat } from './enums/DataFormat';
+import { DelegateType } from './enums/DelegateType';
+import { FieldStatus } from './enums/FieldStatus';
+import { FieldType } from './enums/FieldType';
+import { FileStorageSite } from './enums/FileStorageSite';
+import { FilterOperators } from './enums/FilterOperators';
+import { FormulaOutputType } from './enums/FormulaOutputType';
+import { HttpStatusCode } from './enums/HttpStatusCode';
+import { Multiplicity } from './enums/Multiplicity';
+import { RecordValueType } from './enums/RecordValueType';
+import { ReportDataType } from './enums/ReportDataType';
+import { TimeSpanIncrement } from './enums/TimeSpanIncrement';
+import { TimeSpanRecurrenceType } from './enums/TimeSpanRecurrenceType';
+
+export {
+  // Models
+  ApiResponse,
+  App,
+  Attachment,
+  AttachmentListRecordValue,
+  CollectionResponse,
+  CreatedWithIdResponse,
+  DateRecordValue,
+  DecimalRecordValue,
+  Delegate,
+  DelegateListRecordValue,
+  EndpointFactory,
+  Field,
+  File,
+  FileInfo,
+  FileListRecordValue,
+  FormulaField,
+  GetPagedAppsResponse,
+  GetPagedFieldsResponse,
+  GetPagedRecordsResponse,
+  GetPagedReportsResponse,
+  GetRecordRequest,
+  GetRecordsByAppIdRequest,
+  GetRecordsRequest,
+  GuidListRecordValue,
+  GuidRecordValue,
+  IntegerListRecordValue,
+  IntegerRecordValue,
+  ListField,
+  ListItemRequest,
+  ListItemResponse,
+  ListValue,
+  OnspringClient,
+  PagedResponse,
+  PagingRequest,
+  QueryFilter,
+  QueryRecordsRequest,
+  Record,
+  RecordValue,
+  ReferenceField,
+  Report,
+  ReportData,
+  Row,
+  SaveFileRequest,
+  SaveRecordRequest,
+  SaveRecordResponse,
+  ScoringGroup,
+  ScoringGroupListRecordValue,
+  StringListRecordValue,
+  StringRecordValue,
+  TimeSpanData,
+  TimeSpanRecordValue,
+  // Enums
+  DataFormat,
+  DelegateType,
+  FieldStatus,
+  FieldType,
+  FileStorageSite,
+  FilterOperators,
+  FormulaOutputType,
+  HttpStatusCode,
+  Multiplicity,
+  RecordValueType,
+  ReportDataType,
+  TimeSpanIncrement,
+  TimeSpanRecurrenceType,
+};
