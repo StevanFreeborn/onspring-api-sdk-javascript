b6bc26eabdace4a4244cf058b25f010bb634bc7d
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-02-17 00:32:11 -0600

    fix: add timeout and retry settings to canConnect integration test

--- integrationTests/Ping/canConnect.spec.ts
+++ integrationTests/Ping/canConnect.spec.ts
@@ -3,6 +3,9 @@ import { expect } from 'chai';
 import { baseURL, apiKey } from '../mochaRootHooks';
 
 describe('canConnect', function () {
+  this.timeout(30000);
+  this.retries(3);
+
   it('should be able to connect to the API', async function () {
     const client = new OnspringClient(baseURL, apiKey);
     const response = await client.canConnect();
